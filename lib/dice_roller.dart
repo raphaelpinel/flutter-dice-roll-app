78638b43e158d5a11a8ead650fcca353354589e3
Raphael Pinel <Raphael.Pinel@knowit.fi>
2023-12-05 20:28:28 +0200

    Add dice roller

---
+++ lib/dice_roller.dart
@@ -0,0 +1,42 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() => _DiceRollerState();
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var diceNumber = 2;
+
+  void rollDice() {
+    setState(() {
+      diceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/dice-$diceNumber.png'),
+          const SizedBox(height: 16.0),
+          TextButton(
+            onPressed: rollDice,
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(fontSize: 24),
+            ),
+            child: Text('Roll Dice'),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:first_app/dice_roller.dart';
 import 'package:flutter/material.dart';
 import 'package:first_app/gradient_container.dart';
-import 'package:first_app/styled_text.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -12,9 +12,7 @@ void main() {
         ],
         startAlignment: Alignment.topRight,
         endAlignment: Alignment.bottomLeft,
-        child: StyledText(
-          'Hello World!',
-        ),
+        child: DiceRoller(),
       ),
     ),
   ));
